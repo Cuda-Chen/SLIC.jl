66ad258a8c92a6af8116e59ccaed0eb2db439396
Cuda-Chen <clh960524@gmail.com>
2020-07-21 16:45:22 +0800

    Learning how to use JuliaImage
    1. Learning how to use JuliaImage, and I hate it a lot :)
    2. Add some lines in SLIC.jl.
    3. Make some changes in Project.toml.

--- demo.jl
+++ demo.jl
@@ -1,5 +1,9 @@
-using Images, ImageView
+using Images, ImageView, ImageCore
 
-input_image = load("lenna.bmp")
+input_image = load("dog.png")
 #imshow(input_image)
-mosaicview(input_image)
+#mosaicview(input_image)
+#save("foo.bmp", input_image)
+println(size_spatial(input_image)) # get the size of an image
+println(size_spatial(input_image)[1])
+println(size_spatial(input_image)[2])
--- src/SLIC.jl
+++ src/SLIC.jl
@@ -1,1 +1,16 @@
 using Images
+
+struct Cluster
+    l
+    a
+    b
+    x
+    y
+    index
+end
+
+function slic(img, K, M)
+    size_tuple = size_spatial(img)
+    image_height = size_tuple[1]
+    image_width = size_tuple[2] 
+end
