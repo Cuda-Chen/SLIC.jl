3a7187378dcafad8fdf2a15cf824d7506dbdea2a
Cuda-Chen <clh960524@gmail.com>
2020-07-21 16:55:06 +0800

    finish paramters initialization in SLIC.jl

--- src/SLIC.jl
+++ src/SLIC.jl
@@ -9,8 +9,15 @@ struct Cluster
     index
 end
 
-function slic(img, K, M)
+function slic(img, K, M, iterations=10)
     size_tuple = size_spatial(img)
     image_height = size_tuple[1]
     image_width = size_tuple[2] 
+    S = Integer(sqrt((image_height * image_width) / K))
+    clusters = Cluster[]
+    labels = Dict()
+    distance = Matrix(Inf, image_height, image_width)
+
+    # Initialize each cluster
+    
 end
