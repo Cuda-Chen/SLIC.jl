a6836185836e6db5663e58fb2d723256424e3575
Cuda-Chen <clh960524@gmail.com>
2020-07-21 17:25:49 +0800

    finish initialize cluster part in SLIC.jl
    1. Finish initialize cluster part in SLIC.jl.
    2. Working on move cluter center to lowest gradient part.

--- src/SLIC.jl
+++ src/SLIC.jl
@@ -6,10 +6,11 @@ struct Cluster
     b
     x
     y
-    index
+    cluster_index
 end
 
 function slic(img, K, M, iterations=10)
+    img_lab = Lab.(img)
     size_tuple = size_spatial(img)
     image_height = size_tuple[1]
     image_width = size_tuple[2] 
@@ -18,6 +19,23 @@ function slic(img, K, M, iterations=10)
     labels = Dict()
     distance = Matrix(Inf, image_height, image_width)
 
-    # Initialize each cluster
-    
+    # Initialize each cluster and its fields
+    idx = 1
+    for x = div(S, 2):S:image_width
+        for y = div(S, 2):S:image_height
+            clusters.push!(Cluster(img_lab[y][x][1],
+                                   img_lab[y][x][2],
+                                   img_lab[y][x][3],
+                                   x,
+                                   y,
+                                   idx))
+            idx += 1
+        end
+    end
+
+    # Move the center of each cluster to the local lowgest gradient position
+    for cluster in clusters:
+        # Get current gradient of this center
+        ####
 end
+
